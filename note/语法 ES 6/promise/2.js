ae024355f9e2bc73c58e274b78151575535b35b0
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-03 02:11:05 +0800

    then 方法,
    把回调函数的工作，给干了

--- note/语法 ES 6/promise/2.js
+++ note/语法 ES 6/promise/2.js
@@ -3,9 +3,30 @@ let pro = new Promise( (resolve, reject) => {
     setTimeout( () => {
 
         console.log('异步请求 ，   hahaha');
+
+        resolve('下面  then  函数的数据');
     }, 1000 );
 
 
 
 
 });
+
+
+
+
+
+
+pro.then( (data) => { 
+
+
+    console.log('data:  ', data);
+
+},  () => {
+
+
+
+});
+
+
+
