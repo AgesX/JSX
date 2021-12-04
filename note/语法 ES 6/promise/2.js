fb471dad55899aa3e5f8078ed6dbc650659969d6
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-04 11:00:55 +0800

    // Promise, 出错之后，不但会走 catch 方法，
    // 还会走 then 的第二个参数回调

--- note/语法 ES 6/promise/2.js
+++ note/语法 ES 6/promise/2.js
@@ -3,13 +3,15 @@ let pro = new Promise( (resolve, reject) => {
     setTimeout( () => {
 
         console.log('异步请求 ，   hahaha');
+
+        // 1 ～ 10 的，一个随机数
         let time = Math.random() * 10;
 
         if( time < 5 ){
             resolve('下面  then  函数的数据: ' + time);
         }
         else{
-            reject('出现了 错误 ');
+            reject('出现了 (下面  catch  函数的)   错误 : ' + time);
         }
     }, 1000 );
 
@@ -21,18 +23,45 @@ let pro = new Promise( (resolve, reject) => {
 
 
 
+// Promise, 出错之后，不但会走 catch 方法，
 
+// 还会走 then 的第二个参数回调
 
 pro.then( (data) => { 
-
+    // 第一个回调，
+    // 对应上面的 resolve 方法
 
     console.log('data:  ', data);
 
-},  () => {
+},  (ggg) => {
 
-
+    console.log('then 的第二个入参:  ', ggg);
 
 });
+
+
+
+pro.catch( (err) => { 
+    // 对应上面的 reject 方法
+
+    console.log('wrong:  ', err);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
