10e6ae71905384c1e7677652382376bb6f86c202
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-07 00:50:44 +0800

    // 调用 generator 函数，
    // 不会立即执行 generator 函数体，里面的代码

---
+++ note/语法 ES 6/generator/0.js
@@ -0,0 +1,39 @@
+
+// 有一个，星号
+
+function * gen(){
+    console.log('录音');
+
+}
+
+
+
+//
+
+// 使用变量，保存执行 generator 函数，之后的值
+let prepare = gen();
+
+//
+
+// 调用 generator 函数，
+
+// 不会立即执行 generator 函数体，里面的代码
+
+
+//
+
+
+
+
+//  function* + 函数名 + 函数体
+
+
+
+function* genX(){
+
+
+}
+
+
+
+
