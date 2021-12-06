a5ee5245223f07021980b4ba59b97bfde287896d
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-07 00:59:46 +0800

    // value, 和 done //  done , true //  表示，generator 函数， 已经执行完了

--- note/语法 ES 6/generator/0.js
+++ note/语法 ES 6/generator/0.js
@@ -3,7 +3,7 @@
 
 function * gen(){
     console.log('录音');
-
+    yield 660 + 6;
 }
 
 
@@ -25,17 +25,38 @@ let prepare = gen();
 
 
 // 此时， 执行 generator 函数体，里面的代码
-prepare.next();
+let fight = prepare.next();
 
 
 
+// value, 和 done
+//  done , false
+//  表示，generator 函数， 还没有执行完
+
+// 在 yield 语句这里，
+
+// 暂停了
+console.log(fight);
 
 
 
 //
+let warrior = prepare.next();
 
 
 
+// value, 和 done
+//  done , true
+//  表示，generator 函数， 已经执行完了
+
+console.log(warrior);
+
+
+//
+
+//
+
+//
 //  function* + 函数名 + 函数体
 
 
