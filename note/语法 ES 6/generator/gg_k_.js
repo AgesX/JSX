f746bc22c791af8c667064fe2bbfc0e403d54c99
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-10 02:34:12 +0800

    // 是一个  Promise 对象， 的实例

---
+++ note/语法 ES 6/generator/gg_k_.js
@@ -0,0 +1,105 @@
+// 数据竞争的场景
+
+
+function mockAjax(method, url, data){
+
+    return new Promise( (resolve) => {
+
+        //   传递一个箭头函数
+        //   一秒钟之后，执行
+        setTimeout( () => {
+
+                // 字面量对象
+            let result = {
+                status: 666 + "    _     " +url,
+                msg: '干',
+                data: data
+            };
+
+            resolve(result);
+            
+
+        }, 1000)
+
+
+
+    });
+}
+
+
+function * gen(){
+   
+    let getRole = yield mockAjax('GET', '/role?usrID=' + 666,  { kk : 13 } )
+
+    // getRole , 是调用 .next 方法， 传入的 data
+    console.log(getRole);
+    let words = yield mockAjax('GET', '/product?roleK=' + getRole.data.kk,  [{ words: '不要怂' }])
+
+
+    console.log(words);
+}
+
+
+
+
+
+
+let g = gen();
+
+
+
+function run(genX){
+
+
+    let g = genX();
+
+
+    // next 执行之后的，结果
+
+
+    // 自执行，递归函数
+
+
+
+    let result;
+
+
+    (function iterator(params){
+
+
+
+
+        result = g.next();
+
+        // 是一个  Promise 对象， 的实例
+        if( result.value() instanceof  Promise ){
+
+
+
+
+        }
+
+
+
+
+    })();
+
+
+
+
+    return result;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
