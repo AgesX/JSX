33faa6b9ed97aa4bee2bea182fbe9a3162b8c01a
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-28 01:39:04 +0800

    // 函数组件，没有自己的内部状态
    // class 组件， 有自己的状态 state

---
+++ React/note/2.js
@@ -0,0 +1,26 @@
+// class 组件
+
+
+// 使用 class 关键字
+
+
+class Come extends React.Component{
+
+
+    render(){
+
+        return <h1>123    {this.prop.name}</h1>;
+    }
+
+}
+
+
+
+
+
+
+// 函数组件，没有自己的内部状态
+
+
+// class 组件， 有自己的状态 state
+
