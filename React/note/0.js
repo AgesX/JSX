d2883d07cdb7d1e7cc2ba9114ae2122d4ac8d501
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-28 01:26:55 +0800

    React.Fragment  组件

---
+++ React/note/0.js
@@ -0,0 +1,26 @@
+const element = <h1 title="123">456</h1>;
+
+
+const container = document.getElementById('root');
+
+
+ReactDOM.render(element, container);
+
+
+
+
+
+const elementX = <h1>456</h1>;
+
+
+const elementWw = <h1 title="123" className="h1">456</h1>;
+
+
+
+
+const elementZz = <></>;
+
+
+
+
+const elementZzX = <><li>第一行</li><li>第 2 行</li></>;
