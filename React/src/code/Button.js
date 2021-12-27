c66bc9d73fd93cce2740ca1894d65f3893b76066
邓江洲 <dengjiangzhouReal@gmail.com>
2021-12-28 02:28:42 +0800

    // 如果 setState 是覆盖的操作，     // background 属性，就会被 gg

--- React/src/code/Button.js
+++ React/src/code/Button.js
@@ -2,22 +2,29 @@ import React, { Component } from "react";
 
 class Button extends Component {
   state = {
-    color: "red"
+    color: "red",
+    background: "white"
   };
 
+
+  // 点击按钮，变颜色
   onClick = () => {
     // this 这个组件的一个实例
 
+
+    // 如果 setState 是覆盖的操作，
+    // background 属性，就会被 gg
     this.setState({
       color: "black"
     });
+    console.log(this.state.color);
   };
 
   render() {
-    const { color } = this.state;
+    const { color, background } = this.state;
     //  onClick, C 大写， react 的写法
     return (
-      <button style={{ color }} onClick={this.onClick}>
+      <button style={{ color, background }} onClick={this.onClick}>
         {" "}
         点击{" "}
       </button>
@@ -26,3 +33,9 @@ class Button extends Component {
 }
 
 export default Button;
+
+
+
+
+
+
