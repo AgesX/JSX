373952f614f4b0d2cf13b07e60edcfa3d67ad385
邓江洲 <dengjiangzhouReal@gmail.com>
2022-01-06 01:16:34 +0800

    // 如果使用 any, 就是告诉 ts,  关闭类型检查

---
+++ typeScript/2.ts
@@ -0,0 +1,45 @@
+var digit = 11;
+
+
+
+digit = null;
+
+
+
+// null 是所有类型的子类
+
+
+
+// null 可以给任意类型的变量，赋值
+
+
+
+
+
+
+
+
+
+
+// 同 null 一样， 
+
+// undefined  也是所有类型的子类
+
+
+
+
+
+var fight = "ha ha ha";
+
+fight = undefined;
+
+
+
+
+
+// 如果使用 any, 就是告诉 ts,  关闭类型检查
+
+
+
+
+
