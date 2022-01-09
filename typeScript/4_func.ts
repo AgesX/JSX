b928fbed46a6dfa03fe0870e6a4c66d584faa481
邓江洲 <dengjiangzhouReal@gmail.com>
2022-01-10 02:32:44 +0800

    let anonyAddXxx: (xxx: string, yyy: string) => string =

---
+++ typeScript/4_func.ts
@@ -0,0 +1,89 @@
+function combine(a: string, b: string): string{
+
+
+
+    return a + b
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function add(a: string, b: string){
+
+
+
+    return a + b
+
+}
+
+
+
+
+
+
+
+let result = add('st', '   deng')
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+let anonySum = function add(a: string, b: string){
+
+
+
+    return a + b
+
+}
+
+
+
+
+
+
+
+
+
+
+let anonyAdd: (a: string, b: string) => string = function add(a: string, b: string){
+
+
+
+    return a + b
+
+}
+
+
+
+
+let anonyAddXxx: (xxx: string, yyy: string) => string = function add(a: string, b: string){
+
+
+
+    return a + b
+
+}
+
+
+
+
+
+
+
+
