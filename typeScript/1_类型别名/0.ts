27ff7c692069cce9636c6b4e1bf55da58ef5b409
邓江洲 <dengjiangzhouReal@gmail.com>
2022-02-14 02:49:20 +0800

    ts-node   &&   类型 别名

---
+++ typeScript/1_类型别名/0.ts
@@ -0,0 +1,40 @@
+type Name = string
+
+
+
+
+
+
+//              string 类型， 现在多了一个名字
+
+
+
+let fighter: Name = '侠客行'
+
+
+
+
+
+type Man = {
+    name: Name
+}
+
+
+let one: Man = {
+    name: '吴钩'
+    // fears: false
+    // 定义后，不可增加属性
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
