c6f205a6b14926eccee180849ed40fed18835966
邓江洲 <dengjiangzhouReal@gmail.com>
2022-05-11 23:52:59 +0800

    小瑕疵

---
+++ python/selenium/four/run.py
@@ -0,0 +1,19 @@
+from booking.booking import Booking
+
+with Booking(gg=False) as bot:
+    bot.land_first_page()
+    bot.change_currency('CNY')
+    bot.select_to_go('New York')
+    bot.select_date("2022-06-17", "2022-06-18")
+    bot.selectAdults(1)
+    bot.clickSearch()
+    bot.apply_filtrations()
+    print(" exiting ...")
+# context manager
+
+# once Python reaches
+
+# the line outside of the indentation
+
+# then it is going to execute
+# some tear down actions
